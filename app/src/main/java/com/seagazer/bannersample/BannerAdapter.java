6e7b6cded35d8bec834169f1f4b249f3ec0dc4d7
seagazer <seagazer@qq.com>
2020-02-22 10:59:59 +0800

    update multiple item view type sample

--- app/src/main/java/com/seagazer/bannersample/BannerAdapter.java
+++ app/src/main/java/com/seagazer/bannersample/BannerAdapter.java
@@ -1,41 +1,71 @@
 package com.seagazer.bannersample;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class BannerAdapter extends RecyclerView.Adapter<BannerAdapter.BannerHolder> {
+public class BannerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    int[] data = {R.drawable.banner1, R.drawable.banner2, R.drawable.banner3,
+    private String[] textData = {"TEXT_1", "TEXT_2"};
+    private int[] imageData = {R.drawable.banner1, R.drawable.banner2, R.drawable.banner3,
             R.drawable.banner4, R.drawable.banner5};
+    private static final int TYPE_TEXT = 0;
+    private static final int TYPE_IMAGE = 1;
 
     @NonNull
     @Override
-    public BannerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        ImageView itemView = new ImageView(parent.getContext());
-        itemView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        itemView.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        return new BannerHolder(itemView);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == TYPE_TEXT) {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view_text, parent, false);
+            return new TextHolder(itemView);
+        } else {
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view_image, parent, false);
+            return new ImageHolder(itemView);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BannerHolder holder, int position) {
-        holder.imageView.setImageResource(data[position]);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == TYPE_TEXT) {
+            ((TextHolder) holder).textView.setText(textData[position]);
+        }
+        if (getItemViewType(position) == TYPE_IMAGE) {
+            ((ImageHolder) holder).imageView.setImageResource(imageData[position - 2]);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return data.length;
+        return textData.length + imageData.length;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (position < 2) {
+            return TYPE_TEXT;
+        }
+        return TYPE_IMAGE;
+    }
 
-    class BannerHolder extends RecyclerView.ViewHolder {
+    class TextHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        TextHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+        }
+    }
+
+    class ImageHolder extends RecyclerView.ViewHolder {
+
         ImageView imageView;
 
-        BannerHolder(@NonNull View itemView) {
+        ImageHolder(@NonNull View itemView) {
             super(itemView);
             imageView = (ImageView) itemView;
         }
