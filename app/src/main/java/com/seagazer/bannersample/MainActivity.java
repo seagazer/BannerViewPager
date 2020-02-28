b12fe673d326177350a8876f736a178a3448be55
seagazer <kl19901234>
2020-02-28 15:57:12 +0800

    update sample

--- app/src/main/java/com/seagazer/bannersample/MainActivity.java
+++ app/src/main/java/com/seagazer/bannersample/MainActivity.java
@@ -2,6 +2,7 @@ package com.seagazer.bannersample;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +10,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.seagazer.banner.BannerViewPager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private BannerViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BannerViewPager viewPager = findViewById(R.id.banner);
+        viewPager = findViewById(R.id.banner);
         viewPager.setAdapter(new BannerAdapter());
         viewPager.setAutoFlip(true);
         viewPager.setAutoFlipTime(3500);
-        viewPager.setReverseFlip(false);
-        viewPager.setOrientation(RecyclerView.VERTICAL);
         viewPager.setOnPageClickListener(new BannerViewPager.OnPageClickListener() {
             @Override
             public void onPageClick(View view, int position) {
@@ -28,5 +29,35 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+        RadioGroup orientation = findViewById(R.id.orientation);
+        RadioGroup order = findViewById(R.id.order);
+        RadioGroup mode = findViewById(R.id.mode);
+        orientation.setOnCheckedChangeListener(this);
+        order.setOnCheckedChangeListener(this);
+        mode.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.horizontal:
+                viewPager.setOrientation(RecyclerView.HORIZONTAL);
+                break;
+            case R.id.vertical:
+                viewPager.setOrientation(RecyclerView.VERTICAL);
+                break;
+            case R.id.sequence:
+                viewPager.setReverseFlip(false);
+                break;
+            case R.id.reverse:
+                viewPager.setReverseFlip(true);
+                break;
+            case R.id.auto:
+                viewPager.setAutoFlip(true);
+                break;
+            case R.id.hand:
+                viewPager.setAutoFlip(false);
+                break;
+        }
     }
 }
